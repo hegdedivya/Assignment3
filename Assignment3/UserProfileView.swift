95108ea6ba7b9de93ad3fcb3f65cfa047424a41a
hegderdivya <ayvidedgeh@gmail.com>
2025-05-07 17:56:00 +1000

    Basic user profile

--- Assignment3/UserProfileView.swift
+++ Assignment3/UserProfileView.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct UserProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack() {
+            Color.yellow
+                .opacity(0.1)
+                .ignoresSafeArea()
+            VStack() {
+                Text("Profile")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                Image(systemName: "person.crop.circle.fill")
+                    .resizable()
+                    .foregroundColor(.pink)
+                    .frame(width: 120, height: 120)
+                    .padding(.top, 40)
+                    .shadow(radius: 10)
+                
+                // Name & Email
+                Text("John Doe")
+                    .font(.title)
+                    .bold()
+
+                Text("john.doe@example.com")
+                    .foregroundColor(.gray)
+                    .padding()
+                Text("0478937652")
+                Divider().padding(.vertical)
+                Text("SplitMe Pro")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding()
+                Divider().padding(.vertical)
+                Text("Privacy")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding()
+                Divider().padding(.vertical)
+                Text("Contact Us")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding()
+                Divider().padding(.vertical)
+                Text("Rate SplitMe")
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+        }
+        
+        
+        
     }
 }
 
