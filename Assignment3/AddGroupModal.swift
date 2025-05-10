113cb2c9310772fadd98049c0c08afeb12ed567a
Krithik Shetty <krithikshetty92@gmail.com>
2025-05-10 15:53:12 +1000

    lot of changes to add groups, access them from firestore and add users into the group

---
+++ Assignment3/AddGroupModal.swift
@@ -0,0 +1,18 @@
+//
+//  AddGroupModal.swift
+//  Assignment3
+//
+//  Created by Krithik on 10/5/2025.
+//
+
+import SwiftUI
+
+struct AddGroupModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddGroupModal()
+}
---
+++ Assignment3/AddUserToGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  AddUserToGroupView.swift
+//  Assignment3
+//
+//  Created by Krithik on 10/5/2025.
+//
+
+import SwiftUI
+
+struct AddUserToGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddUserToGroupView()
+}
