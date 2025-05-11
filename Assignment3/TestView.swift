69847023fd3ad41dd237be5ba31bfb7c3eb4038b
mk <minkun_he@hotmail.com>
2025-05-11 20:38:16 +1000

    testb fOR GIT

---
+++ Assignment3/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Assignment3
+//
+//  Created by Minkun He on 11/5/2025.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
