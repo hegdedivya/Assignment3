1aaad08541bb0b34b5ba3bc773111810005a2ffe
mk <minkun_he@hotmail.com>
2025-05-11 16:41:23 +1000

    Firebase manager v1 added

---
+++ Assignment3/FireBaseDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  FireBaseDataManager.swift
+//  Assignment3
+//
+//  Created by Minkun He on 11/5/2025.
+//
+
+import SwiftUI
+
+struct FireBaseDataManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FireBaseDataManager()
+}
