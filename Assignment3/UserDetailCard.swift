89226e76dce99f5576733990b8f86b9260551c8c
hegderdivya <ayvidedgeh@gmail.com>
2025-05-11 16:39:32 +1000

    new updated profile view with app theme colors and edit profile view incorporated

---
+++ Assignment3/UserDetailCard.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailCard.swift
+//  Assignment3
+//
+//  Created by Divya on 11/5/2025.
+//
+
+import SwiftUI
+
+struct UserDetailCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailCard()
+}
