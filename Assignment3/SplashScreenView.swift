4e99d0d28e6159160101be10a0b0d87d6ae458e2
mk <minkun_he@hotmail.com>
2025-05-13 17:42:02 +1000

    Have added a splash screen, made sur the app is taken to dashboard if logged in

---
+++ Assignment3/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  Assignment3
+//
+//  Created by Minkun He on 13/5/2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
